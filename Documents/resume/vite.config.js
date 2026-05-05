904b1b8df075ed5846ba5c76e9d6642bfba71239
Fathi-M1 <fatximahadpoqoqr1@gmail.com>
2026-05-05 16:26:02 +0800

    fix: vite configuration for react-native-web

--- Documents/resume/vite.config.js
+++ Documents/resume/vite.config.js
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const extensions = [
+  '.web.tsx', '.web.ts', '.web.jsx', '.web.js',
+  '.tsx', '.ts', '.jsx', '.js', '.json',
+];
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       'react-native': 'react-native-web',
+    },
+    extensions: extensions,
+  },
+  optimizeDeps: {
+    rolldownOptions: {
+      resolve: {
+        extensions: extensions,
+      }
     },
   },
 })
